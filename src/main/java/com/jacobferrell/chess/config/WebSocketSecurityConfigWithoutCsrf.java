ac4802d144c3724c6f1c616b3bd8b6120cca6fde
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-07-20 21:30:02 -0400

    added websocket security configs

---
+++ src/main/java/com/jacobferrell/chess/config/WebSocketSecurityConfigWithoutCsrf.java
@@ -0,0 +1,24 @@
+package com.jacobferrell.chess.config;
+
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+
+@Configuration
+@ConditionalOnProperty(name = "websocket.csrf.enable", havingValue = "0", matchIfMissing = false)
+public class WebSocketSecurityConfigWithoutCsrf
+    extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+  @Override
+  protected void configureInbound(final MessageSecurityMetadataSourceRegistry messages) {
+    messages.anyMessage().authenticated();
+  }
+
+  @Override
+  protected boolean sameOriginDisabled() {
+    return true;
+  }
+
+}
