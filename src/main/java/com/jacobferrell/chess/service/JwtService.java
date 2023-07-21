75b59939c7de74d4c6199fec945a20b98711700c
jacob-ferrell <michaeljacobferrell@gmail.com>
2023-07-20 21:36:06 -0400

    added some methods from websocket security workaround

--- src/main/java/com/jacobferrell/chess/service/JwtService.java
+++ src/main/java/com/jacobferrell/chess/service/JwtService.java
@@ -7,8 +7,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.webjars.NotFoundException;
@@ -17,20 +20,31 @@ import com.jacobferrell.chess.model.UserDTO;
 import com.jacobferrell.chess.repository.UserRepository;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 @Service
 public class JwtService {
-    
+
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    UserDetailsService userDetailsService;
+
     @Value("${app.jwt.secret}")
     private String SECRET_KEY;
+
+    Logger logger = LoggerFactory.getLogger(JwtService.class);
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -64,17 +78,16 @@ public class JwtService {
     }
 
     public String generateToken(
-        Map<String, Object> extraClaims,
-        UserDetails userDetails
-    ) {
+            Map<String, Object> extraClaims,
+            UserDetails userDetails) {
         return Jwts
-            .builder()
-            .setClaims(extraClaims)
-            .setSubject(userDetails.getUsername())
-            .setIssuedAt(new Date(System.currentTimeMillis()))
-            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
-            .signWith(getSignInKey(), SignatureAlgorithm.HS256)
-            .compact();
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
     public boolean isTokenValid(String token, UserDetails userDetails) {
@@ -102,6 +115,31 @@ public class JwtService {
     private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public boolean validateToken(final String authToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(getSignInKey()).build().parseClaimsJws(authToken);
+            return true;
+        } catch (final MalformedJwtException e) {
+            this.logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (final ExpiredJwtException e) {
+            this.logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (final UnsupportedJwtException e) {
+            this.logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (final IllegalArgumentException e) {
+            this.logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
+
+    public Authentication getAuthentication(final String token) {
+        final UserDetails userDetails = this.userDetailsService
+                .loadUserByUsername(this.extractUsername(token));
+        if (userDetails != null) {
+            return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+        }
+        return null;
     }
 
 }
